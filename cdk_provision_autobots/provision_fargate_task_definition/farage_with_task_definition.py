e8108ba27cf5e6e2eec5f45401dbc0a57b7ff221
jeffry zaman <jeffry.zaman@gmail.com>
2021-01-05 09:55:59 -0500

    tried 2nd commit and no success

--- cdk_provision_autobots/provision_fargate_task_definition/farage_with_task_definition.py
+++ cdk_provision_autobots/provision_fargate_task_definition/farage_with_task_definition.py
@@ -13,49 +13,66 @@ class FargateWithTaskDefinition(core.Stack):
 
         _myVpc = _ec2.Vpc(self, "myVpcID", max_azs=2, nat_gateways=1)
         _ecs_cluster_2 = _ecs.Cluster(self, 'cdkFargateCluster', vpc=_myVpc, cluster_name="CdkFargate_Cluser_2")
-        _UI_SERVICE = 'thiethaa/alc-autobots-ui:latest'
+        _UI_SERVICE = 'thiethaa/alc-autobots-ui'
+        # _CHECKLIST_SERVICE = ''
 
         # ecr.amazonaws.com
         # ecs-tasks.amazonaws.com
         # ecs.amazonaws.com
 
-        _container_image = _ecs.ContainerImage.from_registry(_UI_SERVICE)
-        _task_role = _iam.Role(self,
-                               "taskExeRoleID",
-                               # role_name="ecsTaskExecutionRole",
-                               assumed_by=_iam.ServicePrincipal(service='ecs-tasks.amazonaws.com'))
+        _ecs_patterns.ApplicationLoadBalancedFargateService(self, "fargateServiceSecondtry",
+                                                            cluster=_ecs_cluster_2,
+                                                            cpu=512,
+                                                            desired_count=2,
+                                                            memory_limit_mib=1024,
+                                                            public_load_balancer=True,
+                                                            task_image_options=_ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
+                                                                image=_ecs.ContainerImage.from_registry(_UI_SERVICE)))
 
-        alb_task_image_options = _ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
-            # "taskImageOptions",
-            image=_container_image,
-            container_port=4040,
-            container_name="autobots-UI-container",
-            task_role=_task_role,
-            enable_logging=True)
+    """   
+    _container_image = _ecs.ContainerImage.from_registry(_UI_SERVICE)
+       _task_role = _iam.Role(self,
+                              "taskExeRoleID",
+                              # role_name="ecsTaskExecutionRole",
+                              assumed_by=_iam.ServicePrincipal(service='ecs-tasks.amazonaws.com'))
 
-        # A Fargate service running on an ECS cluster fronted by an application load balancer
-        app_load_balanced_fargate_service = _ecs_patterns.ApplicationLoadBalancedFargateService(self,
-                                                                                                'FargateServiceALB',
-                                                                                                cpu=256,
-                                                                                                memory_limit_mib=512,
-                                                                                                cluster=_ecs_cluster_2,
-                                                                                                desired_count=1,
-                                                                                                # listener_port=4040,
-                                                                                                open_listener=True,
-                                                                                                min_healthy_percent=0,
-                                                                                                protocol=elb.ApplicationProtocol.HTTP,
-                                                                                                service_name='autobots_ui',
 
-                                                                                                task_image_options=alb_task_image_options)
 
-        # app_load_balanced_fargate_service.task_definition.execution_role.role_name("ecsTaskExecutionRole")
+       alb_task_image_options = _ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
+           # "taskImageOptions",
+           image=_container_image,
+           container_port=4040,
+           container_name="autobots-UI-container",
+           task_role=_task_role,
+           enable_logging=True)
 
-        app_load_balancer_dns_name = app_load_balanced_fargate_service.load_balancer.load_balancer_dns_name
+       # A Fargate service running on an ECS cluster fronted by an application load balancer
+       app_load_balanced_fargate_service = _ecs_patterns.ApplicationLoadBalancedFargateService(self,
+                                                                                               'FargateServiceALB',
+                                                                                               cpu=256,
+                                                                                               memory_limit_mib=512,
+                                                                                               cluster=_ecs_cluster_2,
+                                                                                               desired_count=1,
+                                                                                               # listener_port=4040,
+                                                                                               open_listener=True,
+                                                                                               min_healthy_percent=0,
+                                                                                               protocol=elb.ApplicationProtocol.HTTP,
+                                                                                               service_name='autobots_ui',
 
-        alb_cfn_output = core.CfnOutput(self, "albDNSName", value=F"{app_load_balancer_dns_name}",
-                                        description="Application load balancer DNS Name for autobots UI")
-        listener_output = core.CfnOutput(self, "albListener", value=F"{app_load_balanced_fargate_service.listener}",
-                                         description="app_load_balanced_fargate_service.listener")
+                                                                                               task_image_options=alb_task_image_options)
+       
+       
+       
 
-        # _logs.LogGroup(self, "albFaragetTaskDefLogGroup", log_group_name=f"{}",
-        #                removal_policy=core.RemovalPolicy.DESTROY)
+       # app_load_balanced_fargate_service.task_definition.execution_role.role_name("ecsTaskExecutionRole")
+
+       app_load_balancer_dns_name = app_load_balanced_fargate_service.load_balancer.load_balancer_dns_name
+
+       alb_cfn_output = core.CfnOutput(self, "albDNSName", value=F"{app_load_balancer_dns_name}",
+                                       description="Application load balancer DNS Name for autobots UI")
+       listener_output = core.CfnOutput(self, "albListener", value=F"{app_load_balanced_fargate_service.listener}",
+                                        description="app_load_balanced_fargate_service.listener")
+
+       # _logs.LogGroup(self, "albFaragetTaskDefLogGroup", log_group_name=f"{}",
+       #                removal_policy=core.RemovalPolicy.DESTROY)
+"""
